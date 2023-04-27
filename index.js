69850f575d1ef6853a564f04d8c226b24bc98829
Shubham Pal <115461478+Shubham0126@users.noreply.github.com>
2023-04-27 17:46:17 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -17,3 +17,26 @@ if (mq.matches) {
 else {
   $('.navbar').removeClass('bg-dark')
 }
+
+$(".place-carousel").owlCarousel({
+  autoplay: true,
+  smartSpeed: 1500,
+  margin: 30,
+  dots: true,
+  loop: true,
+  center: true,
+  responsive: {
+      0:{
+          items:1
+      },
+      576:{
+          items:1
+      },
+      768:{
+          items:2
+      },
+      992:{
+          items:3
+      }
+  }
+});
